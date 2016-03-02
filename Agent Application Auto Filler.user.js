24fed7e3cf06b946803d621a404fbb72c5b7000d
ed1621 <eduardo.giovanni1621@gmail.com>
2016-03-02 14:25:12 -0700

    How do i version

--- Agent Application Auto Filler.user.js
+++ Agent Application Auto Filler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Agent Application Auto Filler
 // @namespace    */Applicants/CreateApplicant*
-// @version      1.08
+// @version      1.09
 // @description  Automatically fills out an application for you.
 //               Dynamically clears out hidden Bank & Card form items and fills them back in upon becoming visible.
 //               If you check the HasCoApplicant checkbox then Co-Applicants form items will be filled out automatically.
