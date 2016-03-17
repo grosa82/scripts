87bfec64381f50aaf4d0f9df437196f92ea60d72
ed1621 <eduardo.giovanni1621@gmail.com>
2016-03-17 14:00:30 -0600

    Created AddCoapp script

---
+++ AddCoapplicant.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Add Coapplicant
+// @namespace    */Applicants/CreateCoApplicant/*
+// @version      0.1
+// @description  Autofill fields when creating a coapplicant on the retailer and agent portal
+// @author       Eduardo Martinez
+// @updateURL    https://github.com/emartinez1621/scripts/raw/master/Update Url
+// @match        */Applicants/CreateCoApplicant/*
+// @match        */Dealers/CreateCoApplicant/*
+// @exclude      https://*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+// Your code here...
