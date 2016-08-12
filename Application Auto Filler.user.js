a0122a5f949bbe6804c694732314f73e7ca8473f
emartinez1621 <emartinez@crestfinancial.com>
2016-08-11 22:26:45 -0600

    I forgot how to version

--- Application Auto Filler.user.js
+++ Application Auto Filler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Application Auto Filler
 // @namespace    */Applicants/CreateApplicant/*
-// @version      6.0
+// @version      6.5
 // @description  Automatically fills out an application for you with the option to fill out the Co-Applicant.
 //               Dynamically clears out hidden Bank & Card form items and fills them back in upon becoming visible.
 //               When Has Co-Applicant checkbox is deselected after initial page load, the Co-Applicant form items are cleared out.
