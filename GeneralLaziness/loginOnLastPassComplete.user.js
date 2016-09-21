63f5d34e0d2bf0c852a05efd0acb5ef300c41395
emartinez1621 <emartinez@crestfinancial.com>
2016-09-21 00:58:39 -0600

    changed file extension, added updateURL, changed version

--- GeneralLaziness/loginOnLastPassComplete.user.js
+++ GeneralLaziness/loginOnLastPassComplete.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Login On LastPass complete;
 // @namespace    emartinez1621
-// @version      0.1
+// @version      1.0
 // @description  This will click the login button when the username and password fields are completed, mainly used for lastpass, cuz im too lazy to hit enter or click the button
 // @author       emartinez1621
 // @match        https://github.com/login
 // @require      https://code.jquery.com/jquery-3.1.0.min.js
+// @updateURL    https://raw.githubusercontent.com/emartinez1621/scripts/master/GeneralLaziness/loginOnLastPassComplete
 // @grant        none
 // ==/UserScript==
 
