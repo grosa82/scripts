78d37c45435cb420fd5d69d3f1c9de7ee84a15f3
emartinez1621 <emartinez@crestfinancial.com>
2016-09-21 00:15:52 -0600

    added update url

--- SkipToLogin.user.js
+++ SkipToLogin.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Skip to Login
 // @namespace    http://github.com/emartinez1621
-// @version      0.1
+// @version      0.2
 // @description  Skip to the login page if on the homescreen ONLY WORKING FOR GITHUB ATM WILL ADD MORE AS I FIND MORE
 // @author       emartinez1621
 // @match        https://github.com/
+// @updateURL    https://github.com/emartinez1621/scripts/raw/master/SkipToLogin.user.js
 // @grant        none
 // ==/UserScript==
 
