52b2a7762bbb63eb9f4a1daace69043495a1cfd5
emartinez1621 <emartinez@crestfinancial.com>
2016-09-24 00:49:35 -0600

    Update RandomGenerator.js

--- RandomGenerator.js
+++ RandomGenerator.js
@@ -1,5 +1,9 @@
+export class RandomGeneration {
+
     randomNumWithXDigits(n) {
         var y = findMaxFromPlace(n);
         var x = findPlace(y);
         return Math.floor(Math.random() * (y - x) + x);
     }
+    
+}
